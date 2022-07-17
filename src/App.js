42f34f0a5e48299e34095a631f700bc26c217024
Pratima <gaitrisanu2468@gmail.com>
2022-07-17 07:03:59 +0545

    add pages
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import {useState} from 'react'
 import './App.css';
 import { BrowserRouter,Route, Routes } from 'react-router-dom'
 import Sidebar from './components/sidebar';
+import Navbar from './components/Navbar';
+import Footer from './components/footer';
 import  Dashboard  from './components/Dashboard/Dashboard.jsx';
 import Agent from './components/Agent/Agent.jsx';
 import  Customer  from './components/Customer/Customer.jsx';
@@ -11,16 +13,30 @@ import  CashTrans  from './components/CashTrans/CashTrans.jsx';
 import  Playing  from './components/Playing/Playing.jsx';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark') 
+    }
+    
+    else {
+      setMode('light')
+    }
+  }
   return (
+    <>
+   <Navbar mode={mode} toggleMode={toggleMode} />
     <div className="App">
      
       <BrowserRouter>
+      
       <Sidebar>
       <Routes>
-      <Route path="/" element={<Dashboard/>}/>
-      <Route path="/agent" element={<Agent/>}/>
-       <Route path="/customer" element={<Customer/>}/> 
-      <Route path="/company" element={<Company/>}/>
+      <Route path="/" element={<Dashboard mode={mode} toggleMode={toggleMode}/>}/>
+      <Route path="/agent" element={<Agent mode={mode} toggleMode={toggleMode}/>}/>
+       <Route path="/customer" element={<Customer mode={mode} toggleMode={toggleMode}/>}/> 
+      <Route path="/company" element={<Company mode={mode} toggleMode={toggleMode}/>}/>
       <Route path="/gametype" element={<Gametype/>}/>
       <Route path="/cashtrans" element={<CashTrans/>}/>
       <Route path="/playing" element={<Playing/>}/>
@@ -28,6 +44,8 @@ function App() {
       </Sidebar>
       </BrowserRouter>
       </div>
+      <Footer mode={mode} toggleMode={toggleMode}/>
+      </>
     
   );
 }
