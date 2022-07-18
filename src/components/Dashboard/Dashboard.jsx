2e3e885407807fc8caabba4c292beede03b04358
Pratima <gaitrisanu2468@gmail.com>
2022-07-18 21:23:53 +0545

    add dashboard
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -5,27 +5,32 @@ import Cards from '../../pages/Cards/cards'
  import Chart from '../../pages/Chart/chart'
 import Featured from '../../pages/featured/featured'
  import Widget from '../../pages/widget/widget'
-
+ import DashboardIcon from '@mui/icons-material/Dashboard';
 import './Dashboard.css'
 
-const Dashboard = () => {
+const Dashboard = (props) => {
   return (
     <>
-    <div className='dashboard'>
-    <div className='heading'>Dashboard</div>
+    <div className={`dashboard dashboard-${props.mode} bg-${props.mode}`}>
+    <div className='heading'>
+      <DashboardIcon/>Dashboard
+    </div>
      <div className='widget'>
      <Cards type="user"/>
      <Cards type="order"/>
      <Cards type="earning"/>
      <Cards type="balance" />
      </div> 
-     <div className='featured'>
+    
+     <div className='feature'>
       <div><Featured/></div>
       <div><Widget/></div> 
-    </div> 
+    </div>
     <div className='chart'>
     <Chart title="Last 6 Months (Revenue)" aspect={2 / 1}/>
     </div> 
+      
+   
     </div>
     </>
   )
