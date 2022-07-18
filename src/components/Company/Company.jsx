2075e91d5b689a108225b47694ae19b88c13ae01
Pratima <gaitrisanu2468@gmail.com>
2022-07-18 21:22:35 +0545

    add list of company
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import './Company.css'
+import React from "react";
+import './Company.css';
+import { companytype } from "../../Data/company"
+import { Icon } from '@iconify/react';
+function Company(props) {
 
-const Company = () => {
   return (
-    <div>Company</div>
-  )
-}
-
+   
+    <div className="company">
+    {
+      companytype.map((item,index)=> {
+        return(
+          <>
+          <div className={`company-container agent-${props.mode} bg-${props.mode}`}>
+        <div className="company-article">
+          <div className="title">{item.title}</div>
+          <div className="content">
+            <div className="industry"><span>Industry</span> <span>: {item.industry}</span></div>
+            <div className="employees"><span>Number of employees</span>  <span> :{item.Numberofemployees}</span></div>
+            <div className="location"><span>Location</span> <span>: {item.Location}</span></div>
+          </div>
+          <div className="details">{item.details}</div>
+          </div>
+        <div className="company-bottom">
+       
+        <Icon icon="typcn:user"width="100" height="100" />
+        <h4> View Top Management Contacts</h4>
+        </div>
+        </div>
+        </> 
+      )})
+    }
+    
+    </div>
+    
+      
+        
+      
+   
+    
+    
+  )}
 export default Company
