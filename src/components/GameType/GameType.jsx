42d67fe7bc882afa0bea70a4dd33ba881ad3bc66
Pratima <gaitrisanu2468@gmail.com>
2022-07-18 21:25:15 +0545

    add game type page
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- src/components/GameType/GameType.jsx
+++ src/components/GameType/GameType.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
-import './GameType.css'
+import React,{useState,useEffect} from "react";
+import './GameType.css';
+import axios from 'axios';
 
-const GameType = () => {
+function GameType(props) {
+  const [news, setNews] = useState([ ]) 
+ 
+  
+  useEffect(() => {
+  axios.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false")
+    .then(res => {
+      console.log(res)
+      setNews(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  })
+
+   
   return (
-    <div>GameType</div>
-  )
-}
-
-export default GameType
+    <>
+    
+    <div className="cont">
+         <div className={`row table-${props.mode} bg-${props.mode}`}>
+   
+    {
+    news.map((value) => {
+      return (
+        <>
+         <div className={`article table-${props.mode} bg-${props.mode}`}> 
+         <div className="art"> 
+        <div class="cryptoid">
+        <h1>{value.id}</h1>
+        <img src={value.image} alt="" />  
+        </div>
+         <div class="box">
+          <p>{value.symbol}</p>
+        <p>{value.name}</p>
+        <p> {value.current_price}</p>
+         </div> 
+         </div>    
+        
+       </div>
+      
+        </>
+        
+      )
+    })
+  }
+  
+  </div>
+       
+       </div>
+    
+    
+    </>
+  )}
+export default GameType;
