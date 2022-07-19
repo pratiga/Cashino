372a4a8e8842ad22597323d0a757e7a671981b40
Pratima <gaitrisanu2468@gmail.com>
2022-07-19 20:46:19 +0545

    overall data
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- src/Data/data.js
+++ src/Data/data.js
@@ -9,7 +9,9 @@ import {
     
 } from "@iconscout/react-unicons";
 
-
+import img1 from '../imgs/img1.jpg'
+import img2 from '../imgs/img2.jpg'
+import img3 from '../imgs/img3.jpg'
 
 // Sidebar Data 
 export const SidebarData = [
@@ -51,3 +53,171 @@ export const SidebarData = [
     
     
 ];
+ export const agentlist = [
+    {
+      agent_id:"1",
+      agent_name:"jone",
+      agent_no:"23478D",
+      country_id:"",
+      fullAddress:"",
+      phone:"0978564",
+      Email:"example@email.com",
+      agent_pic:img1, 
+      amount:"$200",
+      date:"2021-01-12",
+    },
+    {
+      agent_id:"2",
+      agent_name:"jone",
+      agent_no:"23478D",
+      country_id:"",
+      fullAddress:"",
+      phone:"0978564",
+      Email:"example@email.com",
+      agent_pic:img2,
+      amount:"$300",
+      date:"2020-07-22", 
+    },
+    {
+      agent_id:"3",
+      agent_name:"jone",
+      agent_no:"23478D",
+      country_id:"",
+      fullAddress:"",
+      phone:"0978564",
+      Email:"example@email.com",
+      amount:"$290",
+      agent_pic:img3, 
+      date:"2020-01-12",
+    },
+    {
+      agent_id:"4",
+      agent_name:"jone",
+      agent_no:"23478D",
+      country_id:"",
+      fullAddress:"",
+      phone:"0978564",
+      Email:"example@email.com",
+      agent_pic:img1,
+      amount:"$400",
+      date:"2021-01-12", 
+    },
+    {
+      agent_id:"5",
+      agent_name:"jone",
+      agent_no:"23478D",
+      country_id:"",
+      fullAddress:"",
+      phone:"0978564",
+      Email:"example@email.com",
+      agent_pic:img2, 
+      amount:"$500",
+      date:"2020-01-12",
+    },
+    {
+        agent_id:"5",
+        agent_name:"jone",
+        agent_no:"23478D",
+        country_id:"",
+        fullAddress:"",
+        phone:"0978564",
+        Email:"example@email.com",
+        agent_pic:img1, 
+        amount:"$500",
+        date:"2020-01-12",
+      },
+      {
+        agent_id:"5",
+        agent_name:"jone",
+        agent_no:"23478D",
+        country_id:"",
+        fullAddress:"",
+        phone:"0978564",
+        Email:"example@email.com",
+        agent_pic:img2, 
+        amount:"$500",
+        date:"2020-01-12",
+      },
+      {
+        agent_id:"5",
+        agent_name:"jone",
+        agent_no:"23478D",
+        country_id:"",
+        fullAddress:"",
+        phone:"0978564",
+        Email:"example@email.com",
+        agent_pic:img3, 
+        amount:"$500",
+        date:"2020-01-12",
+      },
+      {
+        agent_id:"5",
+        agent_name:"jone",
+        agent_no:"23478D",
+        country_id:"",
+        fullAddress:"",
+        phone:"0978564",
+        Email:"example@email.com",
+        agent_pic:img2, 
+        amount:"$500",
+        date:"2020-01-12",
+      },
+      {
+        agent_id:"5",
+        agent_name:"jone",
+        agent_no:"23478D",
+        country_id:"",
+        fullAddress:"",
+        phone:"0978564",
+        Email:"example@email.com",
+        agent_pic:img1, 
+        amount:"$500",
+        date:"2020-01-12",
+      },
+      {
+        agent_id:"5",
+        agent_name:"jone",
+        agent_no:"23478D",
+        country_id:"",
+        fullAddress:"",
+        phone:"0978564",
+        Email:"example@email.com",
+        agent_pic:img3, 
+        amount:"$500",
+        date:"2020-01-12",
+      },
+      {
+        agent_id:"5",
+        agent_name:"jone",
+        agent_no:"23478D",
+        country_id:"",
+        fullAddress:"",
+        phone:"0978564",
+        Email:"example@email.com",
+        agent_pic:img1, 
+        amount:"$500",
+        date:"2020-01-12",
+      },
+  ]
+ 
+  export const UpdatesData = [
+    {
+        img: img1,
+        name: "Andrew Thomas",
+        noti: "has ordered Apple smart watch 2500mh battery",
+        time: "25 seconds ago",
+    },
+    {
+        img: img2,
+        name: "james Bond",
+        noti:"has received Samsung gadget for charging battery",
+        time:"30 minute ago",
+    },
+    {
+        img: img3,
+        name:"Iron Man",
+        noti:"has ordered Apple smart watch, samsung Gear 2500mh battery",
+        time:"2 hours ago",
+    },
+
+]
