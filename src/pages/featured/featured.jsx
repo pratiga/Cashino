05748f6fd1123357790bf3d13b1cb7152bb137b1
Pratima <gaitrisanu2468@gmail.com>
2022-07-20 21:57:31 +0545

    add featured page
    Signed-off-by: Pratima <gaitrisanu2468@gmail.com>

--- src/pages/featured/featured.jsx
+++ src/pages/featured/featured.jsx
@@ -16,7 +16,7 @@ const Featured = () => {
         <div className="featuredChart">
           <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
         </div>
-        <p className="title">Total sales made today</p>
+        <p className="title">Total income made today</p>
         <p className="amount">$420</p>
         <p className="desc">
           Previous transactions processing. Last payments may not be included.
